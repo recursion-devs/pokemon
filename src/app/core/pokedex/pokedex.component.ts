4afa23f45754dfc8c2248582207f7b36a542fb22
Ezekiel Joshua Cajano <ejcajano11@gmail.com>
2020-01-27 13:38:21 +0800

    fix data

--- src/app/core/pokedex/pokedex.component.ts
+++ src/app/core/pokedex/pokedex.component.ts
@@ -3,20 +3,23 @@ import { POKEDEX } from '@shared/objects/pkdex'
 import { ApiService } from '@shared/services/api.service'
 import { RouterModule, Routes,Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-pokedex',
   templateUrl: './pokedex.component.html',
   styleUrls: ['./pokedex.component.scss']
 })
 export class PokedexComponent implements OnInit {
+  showContent = ''
   pokedex=POKEDEX
   image:string[]=[]
   index=0
   data
   str="https://32wwqvjn96.execute-api.ap-southeast-1.amazonaws.com/dev/pkdex/pkmon/"
 
-  constructor(private api:ApiService, private router:Router) { }
+  constructor(
+    private api:ApiService,
+    private router:Router,
+  ) { }
 
   ngOnInit() {
     this.pokedex.forEach(pokemon => {
@@ -24,28 +27,42 @@ export class PokedexComponent implements OnInit {
       let imgLink ="https://galardex.s3-ap-southeast-1.amazonaws.com/pkimage/"+pokemonId+".png"
 
       pokemon["img_link"] = imgLink
+      pokemon["show_content"] = 'mainData'
     });
 
-    console.log(this.pokedex)
+    // console.log(this.pokedex)
+
+    // this.showContent = 'mainData';
   }
 
-changeHeading(){
-  document.getElementById('type').style.background='yellow';
-  console.log('Yellow')
-}
+  changeHeading(){
+    document.getElementById('type').style.background='yellow';
+    console.log('Yellow')
+  }
 
+  async getPokemonData(id){
+    var pokemonID
+    var pokemonData
+    pokemonID=id.replace(/#/g,"")
+    pokemonData= await this.api.getPokemonData(this.str,pokemonID).toPromise()
+    console.log(pokemonData)
+    this.router.navigateByUrl('/profile')
+  }
 
+  onClickOffense(pokemon){
+    let index = this.pokedex.indexOf(pokemon);
+    this.pokedex[index]["show_content"] = 'offenseData';
+  }
 
-async getPokemonData(id){
-  var pokemonID
-  var pokemonData
-  pokemonID=id.replace(/#/g,"")
-  pokemonData= await this.api.getPokemonData(this.str,pokemonID).toPromise()
-  console.log(pokemonData)
-  this.router.navigateByUrl('/profile')
-}
+  onClickDefender(pokemon){
+    let index = this.pokedex.indexOf(pokemon);
+    this.pokedex[index]["show_content"] = 'defenderData';
+  }
 
-
+  onClickReturn(pokemon){
+    let index = this.pokedex.indexOf(pokemon);
+    this.pokedex[index]["show_content"] = 'mainData';
+  }
 }
 
 
